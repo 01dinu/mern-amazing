c535cf6527dea0e53cc2a79821f816707af409ec
01dinu <90330425+01dinu@users.noreply.github.com>
2023-03-03 19:37:09 +0530

    product list created

---
+++ frontend/src/data.js
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      name: 'Tiered Cotton Midi Dress',
+      slug: 'tiered-cotton-midi -dress',
+      category: 'Dresses',
+      image: '/images/pa.jpg',
+      price: 3200,
+      countInStock: 10,
+      brand: 'Calson',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Gauze Vacation Shirtdress',
+      slug: 'gauze-vacation-shirtdress',
+      category: 'Dresses',
+      image: '/images/pb.jpg',
+      price: 4720,
+      countInStock: 10,
+      brand: 'Calson',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality dress',
+    },
+    {
+      name: 'Mixed Floral Cotton Maxi',
+      slug: 'mixed-floral-cotton-maxi',
+      category: 'Dresses',
+      image: '/images/pc.jpg', //679px x 829px
+      price: 5220,
+      countInStock: 10,
+      brand: 'Calson',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality dress',
+    },
+    {
+      name: ' Sleeve Shirtdress',
+      slug: 'sleeve-shirtdress',
+      category: 'Dresses',
+      image: '/images/pd.jpg',
+      price: 3290,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality dress',
+    },
+  ],
+};
+export default data;
